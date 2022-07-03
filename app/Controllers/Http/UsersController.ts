8050e8b1f5144e79f1806e54df1772fa9a855cc9
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-03 12:14:28 -0400

    user login & signup

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,27 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
+import authConfig from 'Config/auth';
 
-export default class UsersController {}
+export default class UsersController {
+	public async signup({ request, auth }: HttpContextContract) {
+		const name: string = request.input('name');
+		const email: string = request.input('email');
+		const password: string = request.input('password');
+
+		// in case user already exists
+		const user = User.findBy('email', email);
+
+		// if it's a new user
+		if (!user) await User.create({ name, email, password });
+
+		const token = await auth.use('api').attempt(email, password);
+		return token;
+	}
+
+	public async login({ request, auth }: HttpContextContract) {
+		const email: string = request.input('email');
+		const password: string = request.input('password');
+		const token = await auth.use('api').attempt(email, password);
+		return token;
+	}
+}
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -2,12 +2,21 @@ import { DateTime } from 'luxon';
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
 
 export default class User extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number;
+	@column({ isPrimary: true })
+	public id: number;
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
+	@column()
+	public email: string;
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+	@column()
+	public name: string;
+
+	@column({ serializeAs: null })
+	public password: string;
+
+	@column.dateTime({ autoCreate: true })
+	public createdAt: DateTime;
+
+	@column.dateTime({ autoCreate: true, autoUpdate: true })
+	public updatedAt: DateTime;
 }
