03857edf52f89dbebf02da1c1602011253f19af9
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-07 18:12:19 -0400

    delete & show workouts

--- app/Controllers/Http/WorkoutsController.ts
+++ app/Controllers/Http/WorkoutsController.ts
@@ -9,13 +9,32 @@ export default class WorkoutsController {
 		return workouts.map((workout) => workout.toJSON());
 	}
 
+	public async show({ params }: HttpContextContract) {
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		const { user_id, id } = params;
+		const user = await User.findOrFail(user_id);
+		return await user.related('workouts').query().where('id', id).firstOrFail();
+	}
+
 	public async store({ auth, request }: HttpContextContract) {
 		const { name, template } = await request.validate(CreateWorkout);
 		await auth.use('api').authenticate();
 		if (!auth.user) return;
-		return await auth.user?.related('workouts').create({
-			name,
-			template: Boolean(template),
-		});
+		return (
+			await auth.user.related('workouts').create({
+				name,
+				template: Boolean(template),
+			})
+		).toJSON();
+	}
+
+	public async destroy({ params, auth, response }: HttpContextContract) {
+		const { id } = params;
+		await auth.use('api').authenticate();
+		if (!auth.user) return;
+		const workout = await auth.user.related('workouts').query().where('id', id).firstOrFail();
+		await workout.delete();
+		response.status(204);
+		return;
 	}
 }
