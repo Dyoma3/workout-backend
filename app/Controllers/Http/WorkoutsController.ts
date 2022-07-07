7b5e10bffb863326d0cce9976b6b36116837597a
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-07 16:46:06 -0400

    create workout

--- app/Controllers/Http/WorkoutsController.ts
+++ app/Controllers/Http/WorkoutsController.ts
@@ -1,10 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import User from 'App/Models/User';
+import CreateWorkout from 'App/Validators/CreateWorkoutValidator';
 
 export default class WorkoutsController {
 	public async index({ params }: HttpContextContract) {
 		const user = await User.findOrFail(params.user_id);
 		const workouts = await user.related('workouts').query();
 		return workouts.map((workout) => workout.toJSON());
+	}
+
+	public async store({ auth, request }: HttpContextContract) {
+		const { name, template } = await request.validate(CreateWorkout);
+		await auth.use('api').authenticate();
+		if (!auth.user) return;
+		return await auth.user?.related('workouts').create({
+			name,
+			template: Boolean(template),
+		});
 	}
 }
---
+++ app/Validators/CreateWorkoutValidator.ts
@@ -0,0 +1,13 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+export default class CreateWorkoutValidator {
+	constructor(protected ctx: HttpContextContract) {}
+
+	public schema = schema.create({
+		name: schema.string([rules.trim()]),
+		template: schema.boolean.optional(),
+	});
+
+	public messages: CustomMessages = {};
+}
