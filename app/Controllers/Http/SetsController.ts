8a276af813e45f52b1974b04f80f052e988949cf
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-11 18:28:04 -0400

    delete sets

--- app/Controllers/Http/SetsController.ts
+++ app/Controllers/Http/SetsController.ts
@@ -1,21 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import CreateSet from 'App/Validators/CreateSetValidator';
 import UpdateSet from 'App/Validators/UpdateSetValidator';
+import User from 'App/Models/User';
+import DeleteSet from 'App/Validators/DeleteSetValidator';
+
+async function getWorkoutExercise(user: User, workoutId: number, exerciseId: number) {
+	const workout = await user.related('workouts').query().where({ id: workoutId }).firstOrFail();
+	return await workout.related('workoutExercises').query().where({ exerciseId }).firstOrFail();
+}
 
 export default class SetsController {
 	public async store({ auth, request, params, response }: HttpContextContract) {
 		const { workout_id: workoutId } = params;
 		const { weight, reps, exerciseId } = await request.validate(CreateSet);
-		const workout = await auth
-			.user!.related('workouts')
-			.query()
-			.where({ id: workoutId })
-			.firstOrFail();
-		const workoutExercise = await workout
-			.related('workoutExercises')
-			.query()
-			.where({ exerciseId })
-			.firstOrFail();
+		const workoutExercise = await getWorkoutExercise(auth.user!, workoutId, exerciseId);
 		const set = await workoutExercise.related('sets').create({ weight, reps });
 		response.status(201);
 		return set.toJSON();
@@ -24,23 +22,19 @@ export default class SetsController {
 	public async update({ auth, request, params }: HttpContextContract) {
 		const { id, workout_id: workoutId } = params;
 		const { weight, reps, exerciseId } = await request.validate(UpdateSet);
-		const workout = await auth
-			.user!.related('workouts')
-			.query()
-			.where({ id: workoutId })
-			.firstOrFail();
-		const workoutExercise = await workout
-			.related('workoutExercises')
-			.query()
-			.where({ exerciseId })
-			.firstOrFail();
+		const workoutExercise = await getWorkoutExercise(auth.user!, workoutId, exerciseId);
 		const set = await workoutExercise.related('sets').query().where({ id }).firstOrFail();
 		set.merge({ weight, reps });
 		await set.save();
 		return set.toJSON();
 	}
 
-	public async destroy({ response }: HttpContextContract) {
+	public async destroy({ auth, params, request, response }: HttpContextContract) {
+		const { id, workout_id: workoutId } = params;
+		const { exerciseId } = await request.validate(DeleteSet);
+		const workoutExercise = await getWorkoutExercise(auth.user!, workoutId, exerciseId);
+		const set = await workoutExercise.related('sets').query().where({ id }).firstOrFail();
+		await set.delete();
 		response.status(204);
 		return;
 	}
