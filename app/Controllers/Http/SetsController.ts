0af01ce87b8fb728021933218e016ed330cf1c7a
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-11 16:35:41 -0400

    create sets

--- app/Controllers/Http/SetsController.ts
+++ app/Controllers/Http/SetsController.ts
@@ -1,3 +1,32 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import CreateSet from 'App/Validators/CreateSetValidator';
+import Set from 'App/Models/Set';
 
-export default class SetsController {}
+export default class SetsController {
+	public async store({ auth, request, params, response }: HttpContextContract) {
+		const { workout_id: workoutId } = params;
+		const { weight, reps, exerciseId } = await request.validate(CreateSet);
+		const workout = await auth
+			.user!.related('workouts')
+			.query()
+			.where({ id: workoutId })
+			.firstOrFail();
+		const workoutExercise = await workout
+			.related('workoutExercises')
+			.query()
+			.where({ exerciseId })
+			.firstOrFail();
+		const set = await workoutExercise.related('sets').create({ weight, reps });
+		response.status(201);
+		return set.toJSON();
+	}
+
+	public async update({}: HttpContextContract) {
+		return;
+	}
+
+	public async destroy({ response }: HttpContextContract) {
+		response.status(204);
+		return;
+	}
+}
